6b0197afe0a94afc13f42dbd71390888f57deb17
Luca Calzi <lcalzi@axterisco.it>
2025-03-28 09:55:03 +0100

    Lightbox e zoom/seleziona

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { cartActions } from "../repositories/cart/cart-slice";
+import Lightbox from "yet-another-react-lightbox";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+
 import styles from "./ImageGallery.module.css";
 
 export default function ImageGallery({ images }) {
   const dispatch = useDispatch();
   const photoItems = useSelector((state) => state.cart.items);
+  const [open, setOpen] = useState(false);
+  const [index, setIndex] = useState(0);
 
   const handleImageClick = (imageId) => {
     if (photoItems.some((element) => element.id === imageId)) {
@@ -15,21 +23,82 @@ export default function ImageGallery({ images }) {
     }
   };
 
+  const zoom = (i) => {
+    setIndex(i);
+    setOpen(true);
+  };
+
+  const slides = images.map((img) => ({ src: img.src }));
+
   return (
-    <div className="row row-cols-3 row-cols-md-4 row-cols-lg-5 gx-0">
-      {images.map((image) => (
-        <div className="gallery">
-          <div key={image.id} onClick={() => handleImageClick(image.id)} className="ratio ratio-2-3 bg-white">
-            <img
-              src={image.src}
-              alt={`Image ${image.id}`}
-              className={`${styles.foto} ${photoItems.some((element) => element.id === image.id)
-                  ? styles.selected
-                  : ""}`}
-            />
+    <>
+      <div className="row row-cols-3 row-cols-md-4 row-cols-lg-5 gx-0">
+        {images.map((image, i) => (
+          <div className="gallery" key={image.id}>
+            <div
+              onClick={() =>
+                photoItems.length === 0 ? zoom(i) : handleImageClick(image.id)
+              }
+              className="ratio ratio-1-1 bg-white"
+            >
+              <div
+                className={`${styles.foto} ${
+                  photoItems.some((el) => el.id === image.id) ? styles.selected : ""
+                }`}
+                style={{
+                  backgroundImage: `url(${image.src})`,
+                  backgroundRepeat: "no-repeat",
+                  backgroundAttachment: "scroll",
+                  backgroundPosition: "0px 100%",
+                  backgroundSize: "cover",
+                  backgroundColor: "transparent",
+                }}
+              ></div>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+
+      {open && (
+        <Lightbox
+        open={open}
+        close={() => setOpen(false)}
+        index={index}
+        slides={slides}
+        plugins={[Thumbnails]}
+        render={{
+          slideHeader: () => {
+            const image = images[index];
+            const isSelected = photoItems.some((el) => el.id === image.id);      
+            return (
+              <div
+                style={{
+                  position: "absolute",
+                  top: "16px",
+                  right: "80px",
+                  zIndex: 1000,
+                }}
+              >
+                <button
+                  onClick={() => handleImageClick(image.id)}
+                  style={{
+                    padding: "10px 16px",
+                    background: isSelected ? "#dc3545" : "#28a745",
+                    color: "white",
+                    border: "none",
+                    borderRadius: "6px",
+                    fontSize: "14px",
+                    cursor: "pointer",
+                  }}
+                >
+                  {isSelected ? "Rimuovi" : "Seleziona"}
+                </button>
+              </div>
+            );
+          },
+        }}
+      />
+      )}
+    </>
   );
 }
--- src/pages/ImageShop.jsx
+++ src/pages/ImageShop.jsx
@@ -9,14 +9,14 @@ import TotalShopButton from "../components/TotalShopButton";
  */
 export default function ImageShop() {
   const imageList = [
-    { id: 1, src: "Bugatti.jpg" },
-    { id: 2, src: "Lamborghini.jpg" },
-    { id: 3, src: "Opel.jpg" },
-    { id: 4, src: "Opel.jpg" },
-    { id: 5, src: "Opel.jpg" },
-    { id: 6, src: "Opel.jpg" },
-    { id: 7, src: "Opel.jpg" },
-    { id: 8, src: "Opel.jpg" },
+    { id: 1, src: "/tmp/istockphoto-500645381-1024x1024.jpg" },
+    { id: 2, src: "/tmp/istockphoto-535967907-1024x1024.jpg" },
+    { id: 3, src: "/tmp/istockphoto-636828120-1024x1024.jpg" },
+    { id: 4, src: "/tmp/istockphoto-852157310-1024x1024.jpg" },
+    { id: 5, src: "/tmp/istockphoto-936552298-1024x1024.jpg" },
+    { id: 6, src: "/tmp/istockphoto-961494108-1024x1024.jpg" },
+    { id: 7, src: "/tmp/istockphoto-139877917-1024x1024.jpg" },
+    { id: 8, src: "/tmp/istockphoto-1139730571-1024x1024.jpg" },
   ];
 
   return (
