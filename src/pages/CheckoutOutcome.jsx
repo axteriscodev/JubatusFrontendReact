bef01fae7a9f61a263892eb5d600b72b2b0cbdde
Giascar <giascar82@gmail.com>
2025-03-31 16:27:57 +0200

    reindirizzamento da esito checkout

--- src/pages/CheckoutOutcome.jsx
+++ src/pages/CheckoutOutcome.jsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const PAYMENT_COMPLETE = 'complete';
+const PAYMENT_OPEN = 'open';
 
 export default function CheckoutOutcome() {
   const [status, setStatus] = useState(null);
   const orderId = useSelector((state) => state.cart.id);
+  const navigate = useNavigate();
 
+  // fetch dell'esito
   useEffect(() => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
@@ -20,7 +26,28 @@ export default function CheckoutOutcome() {
       });
   }, []);
 
-  if (status === "complete") {
+  // redirect in base all'esito
+  useEffect(() => {
+    if(status) {
+      const timer = setTimeout(() => {
+
+        // il pagamento è completo, vado a richiedere le foto reali
+        if(status === PAYMENT_COMPLETE) {
+          navigate('/processing-photos');
+        }
+
+        // il pagamento risulta ancora in sospeso, ritorno al checkout
+        if(status === PAYMENT_OPEN) {
+          navigate('/checkout');
+        }
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+
+  }, [status]);
+
+  if (status === PAYMENT_COMPLETE) {
     return (
       <>
         <h3>Pagamento completato</h3>
@@ -28,7 +55,7 @@ export default function CheckoutOutcome() {
     );
   }
 
-  if (status === "open") {
+  if (status === PAYMENT_OPEN) {
     return (
       <>
         <h3>Qualcosa è andato storto</h3>
