f23835086cb3540080690070cce03e468a07ab96
Giascar <giascar82@gmail.com>
2025-04-02 15:53:08 +0200

    slicers

--- src/repositories/admin-competitions.js/admin-competitions-slice.js
+++ src/repositories/admin-competitions.js/admin-competitions-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react";
 
 /**
  * Slice per la gestione delle competizioni
@@ -12,9 +13,17 @@ const adminCompetitionsSlice = createSlice({
     setCompetitions(state, action) {
         state.competitions = [...action.payload];
     },
-    addCompetition(state, action) {},
-    editCompetition(state, action) {},
-    deleteCompetition(state, action) {},
+    addCompetition(state, action) {
+      state.competitions.push(action.payload);
+    },
+    editCompetition(state, action) {
+      const indexToEdit = state.competitions.find((item) => item.id === action.payload.id);
+
+      state.competitions[indexToEdit] = action.payload;
+    },
+    deleteCompetition(state, action) {
+      state.competitions = state.competitions.filter((item) => item.id !== action.payload.id);
+    },
     addListToCompetition(state, action) {},
     editListForCompetition(state, action) {},
     deleteListForCompetition(state, action) {},
