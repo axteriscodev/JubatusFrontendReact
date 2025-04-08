5587e7379463789b621a8985d2bbb5486fb37d1a
Giascar <giascar82@gmail.com>
2025-04-08 17:18:01 +0200

    aggiunta apiRequest generica

--- src/services/api-services.js
+++ src/services/api-services.js
@@ -1,12 +1,12 @@
-import { getAuthToken } from '../utils/auth';
+import { getAuthToken } from "../utils/auth";
 
 /**
  * Metodo per invio rischieste API
- * 
+ *
  * @param {*} api - URL dell'API
  * @param {*} method - metodo di richiesta (GET, POST, PUT, DELETE)
  * @param {*} body - body della richiesta
- * @returns 
+ * @returns
  */
 export async function sendRequest(api, method, body) {
   const token = getAuthToken();
@@ -15,8 +15,50 @@ export async function sendRequest(api, method, body) {
     method: method,
     body: body,
     headers: {
-      Authorization : 'Bearer ' + token
-    }
+      Authorization: "Bearer " + token,
+    },
+  });
+
+  if (!response.ok) {
+    console.error("Errore nella richiesta");
+  } else {
+    console.log("Richiesta ok");
+  }
+  return response;
+}
+
+/**
+ * Metodo per invio richieste API
+ *
+ * @param {*} api - endpoint api
+ * @param {*} method - metodo di richiesta (GET, POST, PUT, DELETE)
+ * @param {*} body - body della richiesta
+ * @param {*} needAuth - se la richiesta necessita di autenticazione
+ * @param {*} contentType - tipo di contenuto della richiesta
+ * @returns
+ */
+export async function apiRequest({
+  api,
+  method = "GET",
+  body,
+  needAuth = false,
+  contentType = "application/json",
+}) {
+  const token = getAuthToken();
+  const headers = new Headers();
+
+  if (!(body instanceof FormData)) {
+    headers.append("Content-Type", contentType);
+  }
+
+  if (needAuth) {
+    headers.Authorization = "Bearer " + token;
+  }
+
+  const response = await fetch(api, {
+    method: method,
+    body: body,
+    headers: headers,
   });
 
   if (!response.ok) {
