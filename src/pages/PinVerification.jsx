5861a3b9d39529092b19414c30b7fae803453b90
pmilandri <pmilandri@axterisco.it>
2025-04-15 19:51:14 +0200

    Sistemato bug su import useState

--- src/pages/PinVerification.jsx
+++ src/pages/PinVerification.jsx
@@ -1,4 +1,4 @@
-import { useState, useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { setAuthToken, setLevel } from "../utils/auth";
