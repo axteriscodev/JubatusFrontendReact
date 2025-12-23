2f21d08d0cd5f6931c6511693fc4c07ea5e7186b
Giascar <giascar82@gmail.com>
2025-12-23 17:27:07 +0100

    cambio messaggio e icona

--- src/pages/WorkInProgress.jsx
+++ src/pages/WorkInProgress.jsx
@@ -1,8 +1,12 @@
 const WorkInProgress = () => {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <i
+        className="bi bi-tools"
+        style={{ fontSize: "4rem", color: "#6c757d" }}
+      ></i>
       <h1>Work in Progress</h1>
-      <p>Il servio non è al momento disponibile.</p>
+      <p>We’re making a few improvements. We’ll be back soon!</p>
     </div>
   );
 };
