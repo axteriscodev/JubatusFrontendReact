284f0b5ad24a51500736012c61c487933a86e1cb
Giascar <giascar82@gmail.com>
2025-12-23 17:10:12 +0100

    routing a work in progress

---
+++ src/pages/WorkInProgress.jsx
@@ -0,0 +1,10 @@
+const WorkInProgress = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Work in Progress</h1>
+      <p>Il servio non è al momento disponibile.</p>
+    </div>
+  );
+};
+
+export default WorkInProgress;
