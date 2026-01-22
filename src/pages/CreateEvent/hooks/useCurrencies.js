448db96f2572feb7ca3eafab9a6f7311d7099397
mmichelotti89 <mmichelotti@axterisco.it>
2026-01-22 09:20:38 +0100

    corretta chiamata api currency

--- src/pages/CreateEvent/hooks/useCurrencies.js
+++ src/pages/CreateEvent/hooks/useCurrencies.js
@@ -14,7 +14,7 @@ export function useCurrencies() {
       try {
         setLoading(true);
         const response = await fetch(
-          `${import.meta.env.VITE_API_URL}/contents/currency`
+          `${import.meta.env.VITE_API_URL}/events/currency`,
         );
 
         if (response.ok) {
