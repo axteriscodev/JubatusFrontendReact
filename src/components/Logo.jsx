579de8c5d6bb55de230a178b3dca74bba74e0ea4
mmichelotti89 <mmichelotti@axterisco.it>
2026-01-28 12:12:07 +0100

    aggiunto aspectRatio tra i dati evento e corretti errori

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,9 @@
 import styles from "./Logo.module.css";
 
-export default function Logo({src= "", css = "", size = ""}) {
-    return (<>
-    <img src={src} className={`${styles.logo} ${size} ${css}`}/>
-    </>);
+export default function Logo({ src = "", css = "", size = "" }) {
+  return (
+    <>
+      <img src={src} className={`${styles.logo} ${size} ${css}`} />
+    </>
+  );
 }
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,23 +1,22 @@
 import { useRouteError } from "react-router-dom";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useTranslations } from "../features/TranslationProvider";
 
 export default function ErrorPage() {
   const error = useRouteError();
   const { t } = useTranslations();
 
-  let message = "Si è verificato un errore, si prega di riprovare.";
+  const message =
+    error?.message || "Si è verificato un errore, si prega di riprovare.";
 
-  if (error.data.message) {
-    message = error.data.message;
-  }
+  const status = error?.status;
 
   return (
     <>
       <div className="container">
         <h3>{t("ERROR_WORK")}</h3>
         <p>
-          <strong>Error {error.status ?? ""} - </strong>
+          <strong>Error {status ?? ""} - </strong>
           {message}
         </p>
       </div>
