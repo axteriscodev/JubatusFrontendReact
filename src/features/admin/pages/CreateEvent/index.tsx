3df6b03470adc085d2e7cd5c1bfcacfa2dc9dc45
mmichelotti89 <mmichelotti@axterisco.it>
2026-02-26 09:31:08 +0100

    modifiche su ritorno a lista eventi

--- src/features/admin/pages/CreateEvent/index.tsx
+++ src/features/admin/pages/CreateEvent/index.tsx
@@ -193,7 +193,7 @@ export default function CreateEvent() {
     );
     if (confirmDelete && eventData) {
       dispatch(deleteCompetition(eventData));
-      navigate(ROUTES.ADMIN);
+      navigate(ROUTES.ADMIN_EVENTS);
     }
   };
 
