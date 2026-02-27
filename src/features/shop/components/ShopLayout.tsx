2133991fbef5203dda8fdc61341c4dc4a816f8ee
Giascar <giascar82@gmail.com>
2026-02-27 16:53:06 +0100

    padding laterale su mobile

--- src/features/shop/components/ShopLayout.tsx
+++ src/features/shop/components/ShopLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function ShopLayout() {
   return (
-    <div className="my-10">
+    <div className="my-10 px-4 md:px-0">
       <Outlet />
     </div>
   );
